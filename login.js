6bbf89c6fc900bd77e43b034313584d90c5d73d6
erlynjade <erlynjadedacullo@gmail.com>
2025-10-01 20:54:09 -0700

    Undone
    email and pass still on going

--- login.js
+++ login.js
@@ -1,9 +1,19 @@
-function login(){
-    if (email==="erlynjade@gmail.com" && password==="rlyshin10"
-    )         
-    console.log("your email and password is correct");
-     
 
-         }
- 
+
+//Dacullo, Erlyn Jade P. 1D
+
+function login()
+let email = document.getElementById("email").value;
+let password = document.getElementById("pass").value;
+{
+    console.log("your email and password is correct!");
+} else if("email" && "error-password");{
+    console.log("your password is incorrect");
+}else if("error-email" && "password");{
+    console.log("your email is incorrect");
+}else("error-email" && "error-password");
+    console.log("your email and password is incorrect");
+
+
+
 
