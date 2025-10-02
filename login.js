71440fa3a083a47e38554a10e5a77b28f4765ade
erlynjade <erlynjadedacullo@gmail.com>
2025-10-01 22:34:30 -0700

    Done
    done,  I try hard

--- login.js
+++ login.js
@@ -2,18 +2,32 @@
 
 //Dacullo, Erlyn Jade P. 1D
 
-function login()
+function login(){
+
 let email = document.getElementById("email").value;
 let password = document.getElementById("pass").value;
-{
-    console.log("your email and password is correct!");
-} else if("email" && "error-password");{
-    console.log("your password is incorrect");
-}else if("error-email" && "password");{
-    console.log("your email is incorrect");
-}else("error-email" && "error-password");
-    console.log("your email and password is incorrect");
+console.log("You enter:" + "" + "email");
+console.log("You enter:" + "" + "password");
 
+    if (email==="erlynjadedacullo@gmail.com"){
+        console.log ("Your email is incorrect")
+    }else{
+        console.log ("Your email is incorrect!")
+    }
+
+    if (password==="jadejane2619")
+        console.log ("Your password is correct" +password)
+}else{
+    console.log ("Your password is incorrect!")
+}
+if (login){
+    console.log("Your email and password is incorrect!");
+}else{
+    console.log("Your email and oasswords is correct")
+
+
+}
+    
 
 
 
